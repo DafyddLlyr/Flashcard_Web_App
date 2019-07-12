1c5b6f0265ebd08d94a1a1450e19c3dd471d95ab
dafyddllyr <dafyddllyr@gmail.com>
2019-07-12 23:34:15 +0100

    Card modal basic styling added

--- client/src/components/CardModal.vue
+++ client/src/components/CardModal.vue
@@ -1,10 +1,12 @@
 <template lang="html">
   <div id="background">
-    <div id="card-front">
-      <h2>Front</h2>
-    </div>
-    <div class="card-back">
-      <h2>Back</h2>
+    <div id="card-container">
+      <div class="card front">
+        <h2>Front</h2>
+      </div>
+      <!-- <div class="card back">
+        <h2>Back</h2>
+      </div> -->
     </div>
   </div>
 </template>
@@ -18,13 +20,27 @@ export default {
 <style lang="css" scoped>
 
 #background {
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.7);
   width: 100vw;
   height: 100vh;
   z-index: 2;
   position: absolute;
   top: 0;
   left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-container {
+  perspective: 1000px;
+}
+
+.card {
+  background-color: white;
+  width: 40vw;
+  height: 80vh;
+  border-radius: 20px;
 }
 
 </style>
